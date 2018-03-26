2168bc9c4288d651839a7317852fe245caa1deb8
Michael Pope <popemray1986@gmail.com>
2018-03-26 23:24:26 +0000

    automatically backed up by learn

--- dogs/lib/dogs/cli.rb
+++ dogs/lib/dogs/cli.rb
@@ -14,7 +14,7 @@ class Dogs::CLI
 
   def list_dogs
     counter = 0
-    input = nil
+    input = ""
       Dogs::DogsPopularity.all.each do |dog|
         puts "Which list of dogs would you like to see first 1 or 2"
           input = gets.strip
